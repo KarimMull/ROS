dbd48276d59a2cb261d1a6eaf955a18fe89c6b14
KarimMull <kmullayanov@duck.com>
2026-03-30 15:08:19 +0300

    Добавлен узел time_printer

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'study_pkg'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,17 +13,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='kmull',
-    maintainer_email='kmull@todo.todo',
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
     description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    license='TODO: License',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'first_node = study_pkg.scripts.first_node:main',
         ],
     },
 )
